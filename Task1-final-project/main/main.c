dd263d250db1045bc81df90a831aaac4e322c0bc
Jordan <jordan@pop-os.localdomain>
2023-05-16 14:45:12 -0300

    Commit inicial task 1

---
+++ Task1-final-project/main/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/queue.h>
+#include "driver/gpio.h"
+#include "esp_adc_cal.h"
+
+void app_main(void) {
+
+}
